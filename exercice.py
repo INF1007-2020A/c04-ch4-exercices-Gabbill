459cdd503cdfba3cb20662a54670d5b9ac1b3b09
Gabbill <gabriel.billard@polymtl.ca>
2020-09-15 19:09:38 -0400

    Q1

--- exercice.py
+++ exercice.py
@@ -3,7 +3,11 @@
 
 
 def is_even_len(string: str) -> bool:
-    pass
+    if len(string)%2==0:
+        return True
+    else:
+        return False
+
 
 
 def remove_third_char(string: str) -> str:
@@ -35,7 +39,7 @@ def main() -> None:
     chaine = "hello world!"
     print(f"On remplace le caratère w par le caractère z dans la chaine: {chaine}. Résultat : {replace_char(chaine, 'w', 'z')}")
 
-    print(f"Le nombre d'occurrence de l dans hello est : {get_number_of_char(chaine, 'l')}")
+    print(f"Le nombre d'occurrence de l dans hello world est : {get_number_of_char(chaine, 'l')}")
     
     chaine = "Baby shark doo doo doo doo doo doo"
     print(f"L'occurence du mot doo dans la chaine {chaine} est: {get_number_of_words(chaine, 'doo')}")
